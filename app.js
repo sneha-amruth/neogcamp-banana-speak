37b3be966831cbb649db0fb8b9cddf673974b1d3
Sneha Amruth <sneha.amruth24@gmail.com>
2020-12-09 13:53:22 +0530

    added initial styling

---
+++ app.js
@@ -0,0 +1,9 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var inputTxt = document.querySelector("#input-text");
+var outputTxt = document.querySelector("#output-text");
+
+btnTranslate.addEventListener("click", clickHandler);
+
+function clickHandler(){
+    
+}
